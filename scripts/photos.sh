8d1e14e8a3c1a9d86bfb3795ce0cdf47531ffd24
sohinimallick16 <sohini.mallick@maastrichtuniversity.nl>
2022-10-17 18:44:13 +0200

    multiple scenes files test

--- scripts/photos.sh
+++ scripts/photos.sh
@@ -5,7 +5,7 @@ Reduces quality of images in the media directories
 Do ./install.sh from the same directory first
 """
 
-
+cd data/projects
 for f in ./*/editions/*/articles/media/*.jpg ./*/editions/*/articles/media/*.jpeg  ./*/editions/*/articles/media/*.png
     do
     mogrify -resize 50% $f
