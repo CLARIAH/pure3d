2a980ba2c4a0bc97751939f614da80dc4c63c813
sohinimallick16 <sohini.mallick@maastrichtuniversity.nl>
2022-10-28 16:42:29 +0200

    data changes

---
+++ data/projects/2/editions/1/texts/sources.md
@@ -0,0 +1,1 @@
+Sources appear here
--- scripts/photos.sh
+++ scripts/photos.sh
@@ -5,8 +5,8 @@ Reduces quality of images in the media directories
 Do ./install.sh from the same directory first
 """
 
-cd data/projects
-for f in ./*/editions/*/articles/media/*.jpg ./*/editions/*/articles/media/*.jpeg  ./*/editions/*/articles/media/*.png
+cd ../data/projects
+for f in ./2/editions/2/articles/media/*.JPG ./2/editions/2/articles/media/*.jpeg  ./2/editions/2/articles/media/*.png 
     do
     mogrify -resize 50% $f
     done
