f4ae513c89d443ec696cd0333d69bc7abd19c29b
sohinimallick16 <sohini.mallick@maastrichtuniversity.nl>
2022-11-17 13:50:49 +0100

    tinymce pilot

---
+++ pilots/article-editor/app.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from flask_tinymce import TinyMCE
+from flask import Flask, render_template
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+app = Flask(__name__)
+
+TinyMCE(app) 
+"""
+or
+tinymce = Tinymce()
+tinymce.init_app(app)
+"""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
