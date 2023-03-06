3ed69db4a47ee5da4b521210d44af1b194424af3
sohinimallick16 <sohini.mallick@maastrichtuniversity.nl>
2023-03-06 11:39:31 +0100

    pilot change

--- pilots/testing-users/app.py
+++ pilots/testing-users/app.py
@@ -2,25 +2,38 @@ from flask import Flask, render_template
 
 users = ['user1', 'user2', 'user3','user4']
 
-# create the flask app
-app = Flask(__name__)
+def buttons():
 
-@app.route('/')
-def home():
     html = []
     for user in users:
         html.append(
-            f"""<a href = {user}> <button type="submit" class=cv_btn>{user}</button> </a> """
-        )
+                f"""<a href = {user}> 
+                <button type="submit" class=cv_btn>{user}</button> 
+                </a> 
+                """
+                )
+        
     html = '\n'.join(html) 
-    return render_template('app.html', user=html, user_text =" ")
+    return html
+
+
+def create_app():
+    # create the flask app
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        user = buttons()
+        return render_template('app.html', user=user, user_text=" ")
+
     
-@app.route('/<userN>')
-def userMessage(userN):
-    user_text = f"This is {userN}"
-    return render_template('app.html', user = " ", user_text=user_text )
+    @app.route('/<userN>')
+    def userMessage(userN):
+        user_text = f"This is {userN}"
+        user  = buttons()
+        return render_template('app.html', user=user, user_text=user_text)
+    
+    return app
 
 
-# boilerplate flask app code
-if __name__ == "__main__":
-    app.run(debug=True)
+
