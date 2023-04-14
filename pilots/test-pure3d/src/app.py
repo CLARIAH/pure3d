d13ff7c0b670042f57c9c20274172256a2bd6bcd
sohinimallick16 <sohini.mallick@maastrichtuniversity.nl>
2023-04-14 19:17:02 +0200

    test-pure3d

--- pilots/test-pure3d/src/app.py
+++ pilots/test-pure3d/src/app.py
@@ -1,55 +1,50 @@
 from flask import Flask, render_template, session, url_for, redirect
+from flask_session import Session
 from functions import user_buttons, workflow, editionsList, projectsList, editions_page
 
 app = Flask(__name__)
+app.secret_key = b"1fd10cad570c541436d134d760429d5901edfc71522723ad4e75d2aa0215ef3"
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 users_roles, projects = workflow()
-
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-@app.route("/")
-def index():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
-
-
-@app.route("/home")
-def home():
-    return render_template("index.html", user=user_buttons(), user_text="")
-
-
-@app.route("/<username>")
-def login(username):
-    user_text = f"""
-    {username} is logged in.
-    """
-    return render_template("index.html", user=user_buttons(), user_text=user_text)
 
 
 @app.route("/<username>/login")
-def logging(username):
-    session['username'] = f"{username}"
-    return redirect(url_for('index'))
+def login(username):
+    session["user"] = username
+    user_text = f"""
+            {username} is logged in.
+            """
+    session["user_text"] = user_text
+    return redirect(url_for("home"))
 
 
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
+@app.route("/")
+@app.route("/home")
+def home():
+    user_text = session.get("user_text")
+    return render_template("index.html",
+                           user=user_buttons(),
+                           user_text=user_text)
 
 
 @app.route("/projects")
 def projects():
+    user_text = session.get("user_text")
     projects_list = projectsList()
     return render_template(
-        "projects.html", user=user_buttons(), projects_list=projects_list
+        "projects.html",
+        user=user_buttons(),
+        projects_list=projects_list,
+        user_text=user_text,
     )
 
 
 @app.route("/projects/<project_title>")
 # Display for individual project
 def project_page(project_title):
+    user_text = session.get("user_text")
     projects_user, editions_list = editionsList(project_title)
 
     return render_template(
@@ -57,20 +52,31 @@ def project_page(project_title):
         user=user_buttons(),
         project_user=projects_user,
         editions_list=editions_list,
+        user_text=user_text,
     )
 
 
 @app.route("/projects/<project_title>/<edition_title>")
 def edition_page(project_title, edition_title):
+    user_text = session.get("user_text")
     edition_users = editions_page(project_title, edition_title)
     return render_template(
-        "editionUsers.html", user=user_buttons(), edition_users=edition_users
+        "editionUsers.html",
+        user=user_buttons(),
+        edition_users=edition_users,
+        user_text=user_text,
     )
 
 
 @app.route("/users")
 def users():
-    return render_template("users.html", user=user_buttons(), user_roles=users_roles)
+    user_text = session.get("user_text")
+    return render_template(
+        "users.html",
+        user=user_buttons(),
+        user_roles=users_roles,
+        user_text=user_text
+    )
 
 
 if __name__ == "__main__":
--- pilots/test-pure3d/src/functions.py
+++ pilots/test-pure3d/src/functions.py
@@ -9,12 +9,11 @@ users = ["user1", "user2", "user3", "user4"]
 
 
 def user_buttons():
-
     html = []
     for user in users:
         html.append(
-            f"""<a href = {user}>
-                <button type="submit" class=cv_btn>{user}</button>
+            f"""<a href = "/{user}/login">
+                <button type="submit">{user}</button>
                 </a>
                 """
         )
